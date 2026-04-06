deb15b5a07721ad4fee88cf39a6b66b9fc745753
officialErnests <ernestspursw@gmail.com>
2026-04-06 19:58:31 +0300

    made music thingamajigy

--- views/music/lisen.view.php
+++ views/music/lisen.view.php
@@ -7,19 +7,20 @@
         <p class="main--medium">I have multipe tastes of music, and HUGE insperation from some <span class="randomizer">[^.^]</span></p>
         <p class="main--medium">Here are some of the best I've heard <span class="randomizer">[^.^]</span></p>
         <p class="main--small">btw you can hover some text here <span class="randomizer">[^.^]</span></p>
+        <p class="main--small">also i primarly use yt music, but spotify embeds better... that's why i have both <span class="randomizer">[^.^]</span></p>
         <hr class="main--divider" >
         <div class="main--breaker"></div>
-        <h1 class="main--big">FEMTANYL</h1>
+        <h1 class="main--big">FEMTANYL's era</h1>
         <p class="main--medium">Femtany is one of my favorit artists, and whit the new album
             <span class="lisen__expand">[[MAN BITES THE DOG]]
                 <span class="lisen__expand__hidden lisen__expand__hidden--text">
                     <br>
                     <span class="lisen__expand__hidden--clamp">
                         <img src="/assets/MAN_BITES_DOG.jpg" alt="MAN_BITES_DOG" class="lisen__expand__hidden--img">
-                        <a target="_blank" href="https://music.youtube.com/playlist?list=OLAK5uy_noYatlhKEuUcavpMmmWeB0FuKu74NKooU&si=n6753ZqTpMNXvpKF">
-                            <img src="/assets/YT_music.jpg" alt="yt_music" class="lisen__expand__hidden--ytmusic">
-                        </a>
                     </span>
+                    <a target="_blank" href="https://music.youtube.com/playlist?list=OLAK5uy_noYatlhKEuUcavpMmmWeB0FuKu74NKooU&si=n6753ZqTpMNXvpKF">
+                        <img src="/assets/YT_music.jpg" alt="yt_music" class="lisen__expand__hidden--ytmusic">
+                    </a>
                     MAN BITES DOG by FEMTANYL
                     <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/album/4GRPvwkbXfKXNGfrIJAGCE?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 </span>
@@ -27,9 +28,112 @@
         is insane <span class="randomizer">[^.^]</span></p>
         <p class="main--small">Also will put here the artist that are simmalar, since idk that well music genres <span class="randomizer">[^.^]</span></p>
         <div class="main--breaker"></div>
-        <div class="lisen__grid"></div>
-        //TODO
+        <div class="lisen__grid">
+            <div class="lisen__grid__element">
+                    <span class="lisen__expand__hidden--clamp">
+                    <img src="/assets/MAN_BITES_DOG.jpg" alt="MAN_BITES_DOG" class="lisen__expand__hidden--img">
+                </span>
+                <a target="_blank" href="https://open.spotify.com/artist/53e9n1LucuH3guikmtv180">
+                    <img src="/assets/Spotify.png" alt="spotify" class="lisen__expand__hidden--ytmusic"></a>
+                <a target="_blank" href="https://music.youtube.com/playlist?list=OLAK5uy_noYatlhKEuUcavpMmmWeB0FuKu74NKooU&si=n6753ZqTpMNXvpKF">
+                    <img src="/assets/YT_music.jpg" alt="yt_music" class="lisen__expand__hidden--ytmusic"></a>
+                <h1 class="lisen__grid__element--header">
+                    FEMTANYL
+                </h1>
+                <p class="lisen__grid__elements--subtext">
+                    Is it a suprise? The whole section is labeled Femtanyl <span class="randomizer">[^.^]</span><br><br>
+                    Either way, I've lissened to Femtanyl for a LOOOONG TIME (<?php
+                    $diff = date_diff(date_create(date("d/m/y")), date_create("1/2/2025"));
+                    echo $diff->y;
+                    ?> year, <?= $diff->m?> months and <?= $diff->d?> days to be exact) <br><br>
+                    And it has changed my opinion on music and music creation, which made me create more distored and more daring / challanging sound tracks <span class="randomizer">[^.^]</span>
+                    <br><br>
+                    Here are some of my favorite tracks from em <span class="randomizer">[^.^]</span>
+                </p>
+                <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/album/2AiNSNWXdVa7cWPbhhWYVv?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/5085cUgQnejY42effiVUg9?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/5yMYeobBNjomn5lBwUNDVY?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </div>
+            <div class="lisen__grid__element">
+                    <span class="lisen__expand__hidden--clamp">
+                    <img src="/assets/Stomach_book.jpg" alt="Stomach_book" class="lisen__expand__hidden--img">
+                </span>
+                <a target="_blank" href="https://open.spotify.com/artist/079cEjjSpv5dOLH5xsGdvN">
+                    <img src="/assets/Spotify.png" alt="spotify" class="lisen__expand__hidden--ytmusic"></a>
+                <a target="_blank" href="https://music.youtube.com/channel/UCE8_e3T0JY0zZRMPkdAVFZQ">
+                    <img src="/assets/YT_music.jpg" alt="yt_music" class="lisen__expand__hidden--ytmusic"></a>
+                <h1 class="lisen__grid__element--header">
+                    STOMACH BOOK
+                </h1>
+                <p class="lisen__grid__elements--subtext">
+                    After finding out Femtanyl it didn't took long to be greeted by Stomach book
+                    <br><br>
+                    And it was so beutiful, oh so difrent from femtanyl, but still having the same broken tone to it
+                    <br><br>
+                    It was such a great fun listening to them, such a great time.
+                </p>
+                <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/6RyCpAEHMj1nbopw1g2TsO?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/6XiU3wbMaKkL9vYYj9EgjM?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/0lgeHf116pB2NQewal3keQ?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </div>
+            <div class="lisen__grid__element">
+                    <span class="lisen__expand__hidden--clamp">
+                    <img src="/assets/Fem&m.jpg" alt="Fem&m" class="lisen__expand__hidden--img">
+                </span>
+                <a target="_blank" href="https://open.spotify.com/artist/53e9n1LucuH3guikmtv180">
+                    <img src="/assets/Spotify.png" alt="spotify" class="lisen__expand__hidden--ytmusic"></a>
+                <a target="_blank" href="https://music.youtube.com/playlist?list=OLAK5uy_noYatlhKEuUcavpMmmWeB0FuKu74NKooU&si=n6753ZqTpMNXvpKF">
+                    <img src="/assets/YT_music.jpg" alt="yt_music" class="lisen__expand__hidden--ytmusic"></a>
+                <h1 class="lisen__grid__element--header">
+                    FEM&M
+                </h1>
+                <p class="lisen__grid__elements--subtext">
+                    FEM&M is a great trans artist, whit some insane music. As it was crunch and experimental (and bit like Femtanyls music <span class="randomizer">[^.^]</span>) Altough it was comparably more calmer and more comprehensive<br><br>
+                    At the moment I haven't lissened to Her in a loong time since been listening to something more that screams in my ears <span class="randomizer">[^.^]</span>
+                </p>
+                <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/2zp6aEfkwqZXwBCkSthgIM?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/3qTvGKRsO1fkbFL9KnhRNL?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/4tHawEhoiBAufQ9mOMETFv?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </div>
+            <div class="lisen__grid__element">
+                <span class="lisen__expand__hidden--clamp">
+                    <img src="/assets/Unfamiliar.jpg" alt="Unfamiliar" class="lisen__expand__hidden--img">
+                </span>
+                <a target="_blank" href="https://open.spotify.com/artist/53e9n1LucuH3guikmtv180">
+                    <img src="/assets/Spotify.png" alt="spotify" class="lisen__expand__hidden--ytmusic"></a>
+                <a target="_blank" href="https://music.youtube.com/playlist?list=OLAK5uy_noYatlhKEuUcavpMmmWeB0FuKu74NKooU&si=n6753ZqTpMNXvpKF">
+                    <img src="/assets/YT_music.jpg" alt="yt_music" class="lisen__expand__hidden--ytmusic"></a>
+                <h1 class="lisen__grid__element--header">
+                    (UN)FAMILIAR
+                    <span class="lisen__grid__element--by">
+                        by ME
+                    </span>
+                </h1>
+                <p class="lisen__grid__elements--subtext">
+                    Sub text
+                </p>
+            </div>
+        </div>
     </div>
 </div>
+
+<!-- <div class="lisen__grid__element">
+        <span class="lisen__expand__hidden--clamp">
+        <img src="/assets/MAN_BITES_DOG.jpg" alt="MAN_BITES_DOG" class="lisen__expand__hidden--img">
+    </span>
+    <a target="_blank" href="https://open.spotify.com/artist/53e9n1LucuH3guikmtv180">
+        <img src="/assets/Spotify.png" alt="spotify" class="lisen__expand__hidden--ytmusic"></a>
+    <a target="_blank" href="https://music.youtube.com/playlist?list=OLAK5uy_noYatlhKEuUcavpMmmWeB0FuKu74NKooU&si=n6753ZqTpMNXvpKF">
+        <img src="/assets/YT_music.jpg" alt="yt_music" class="lisen__expand__hidden--ytmusic"></a>
+    <h1 class="lisen__grid__element--header">
+        TITLE
+        <span class="lisen__grid__element--by">
+            by ME
+        </span>
+    </h1>
+    <p class="lisen__grid__elements--subtext">
+        Sub text
+    </p>
+</div> -->
 
 <?php require "./views/components/footer.php" ?>
